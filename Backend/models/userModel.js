a29a81a18182c346050680204ea412e824edf842
Itsmahnor <mahnoorsarfraz23@gmail.com>
2025-07-04 16:31:19 +0500

    add admin frontend

---
+++ Backend/models/userModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+     default:"https://static.vecteezy.com/system/resources/previews/019/879/186/large_2x/user-icon-on-transparent-background-free-png.png"
+    },
+  
+    address: {
+      type: Object,
+      default:{line1:'',line2:''},
+    },
+ gender:{
+    type:String,
+    default:"Not Selected"
+ },
+  dob:{
+    type:String,
+    default:"Not Selected"
+ },
+  phone:{
+    type:String,
+    default:"0000000"
+ }
+  },
+
+);
+
+const userModel =
+  mongoose.models.user || mongoose.model("user", doctorSchema);
+
+export default userModel;
--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-
+import connectDB from './config/mongodb.js'
+import connectcloudinary from './config/cloundinary.js'
+import adminRouter from './routes/adminRoutes.js'
 // app config
 const app = express()
 const port = process.env.PORT || 4000
-
+connectDB()
+connectcloudinary()
 // middlewares
 app.use(express.json())
 app.use(cors())
 
 // api endpoint
+app.use('/api/admin',adminRouter)
+// localhost:4000/api/admin/adddoctor
+
 app.get('/',(req,res)=>{
     res.send('API WORKING Greate!')
 })
